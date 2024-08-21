a4b077bc3d0d6aed97a0ffbf3e1c3545ea537fc4
DESKTOP-4HHRG95\QuangTrung <trungngoquang611@gmail.com>
2024-08-21 12:15:15 +0700

    update

--- components/common/counter/counter.tsx
+++ components/common/counter/counter.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PageCounter() {
     return (
         <>
-            <div>Page Counter</div>
+            <div>Page Counterrr</div>
         </>
     )
 }
