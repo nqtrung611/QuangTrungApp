acbf2c5fa2193f9e781811e6e785320b0e7c2438
DESKTOP-4HHRG95\QuangTrung <trungngoquang611@gmail.com>
2024-08-22 15:49:25 +0700

    update

--- components/common/todo/DeleteForm.tsx
+++ components/common/todo/DeleteForm.tsx
@@ -1,11 +1,9 @@
 import { deleteTask } from "@/utils/actionTodo";
 import React from "react";
 
-const DeleteForm = ({ id }: any) => {
+export default function DeleteForm({ id }: any) {
     return <form action={deleteTask}>
         <input type="hidden" name="id" value={id} />
         <button className="delete">X</button>
     </form>;
 }
-
-export default DeleteForm;
